4c3a963f323c5cf9bab6db7b5a00f0a80f6f755a
DoctorTone <tony@drt-software.com>
2024-05-01 23:26:32 +0100

    Added drt text

--- src/UI/ConfigUI.tsx
+++ src/UI/ConfigUI.tsx
@@ -7,6 +7,7 @@ import {
   RadioGroup,
   Radio,
 } from "@mui/material";
+import Typography from "@mui/material/Typography";
 import useStore from "../state/store";
 import { ChangeEvent } from "react";
 
@@ -24,45 +25,54 @@ const ConfigUI = () => {
   };
 
   return (
-    <div id="configUI" className="panel">
-      <FormGroup>
-        <FormControlLabel
-          control={<Checkbox checked={isRotating} onChange={toggleRotate} />}
-          label="Auto-rotate"
-        />
-        <FormControl sx={{ mt: 3 }}>
-          <FormLabel
-            sx={{ fontWeight: "bold", fontSize: "h6.fontSize" }}
-            id="demo-radio-buttons-group-label"
-          >
-            Pattern:
-          </FormLabel>
-          <RadioGroup
-            aria-labelledby="demo-radio-buttons-group-label"
-            defaultValue="ink"
-            name="radio-buttons-group"
-            onChange={updatePattern}
-          >
-            <FormControlLabel
-              value="ink"
-              control={<Radio />}
-              label="Ink blot"
-            />
-            <FormControlLabel value="zebra" control={<Radio />} label="Zebra" />
-            <FormControlLabel
-              value="flowers"
-              control={<Radio />}
-              label="Flowers"
-            />
-            <FormControlLabel
-              value="orange"
-              control={<Radio />}
-              label="Orange"
-            />
-          </RadioGroup>
-        </FormControl>
-      </FormGroup>
-    </div>
+    <>
+      <div id="configTitle" className="panel">
+        <Typography variant="h5">DRT Software</Typography>
+      </div>
+      <div id="configUI" className="panel">
+        <FormGroup>
+          <FormControlLabel
+            control={<Checkbox checked={isRotating} onChange={toggleRotate} />}
+            label="Auto-rotate"
+          />
+          <FormControl sx={{ mt: 3 }}>
+            <FormLabel
+              sx={{ fontWeight: "bold", fontSize: "h6.fontSize" }}
+              id="demo-radio-buttons-group-label"
+            >
+              Pattern:
+            </FormLabel>
+            <RadioGroup
+              aria-labelledby="demo-radio-buttons-group-label"
+              defaultValue="ink"
+              name="radio-buttons-group"
+              onChange={updatePattern}
+            >
+              <FormControlLabel
+                value="ink"
+                control={<Radio />}
+                label="Ink blot"
+              />
+              <FormControlLabel
+                value="zebra"
+                control={<Radio />}
+                label="Zebra"
+              />
+              <FormControlLabel
+                value="flowers"
+                control={<Radio />}
+                label="Flowers"
+              />
+              <FormControlLabel
+                value="orange"
+                control={<Radio />}
+                label="Orange"
+              />
+            </RadioGroup>
+          </FormControl>
+        </FormGroup>
+      </div>
+    </>
   );
 };
 
