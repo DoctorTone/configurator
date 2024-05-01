5f311580fe1789b445183c10a6c42d50a67ca319
DoctorTone <tony@drt-software.com>
2024-05-01 01:23:31 +0100

    Load vase model

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
 import { Canvas } from "@react-three/fiber";
-import { OrbitControls, Box } from "@react-three/drei";
+import { OrbitControls } from "@react-three/drei";
 import Lights from "./Scenes/Lights";
+import Configurator from "./components/Configurator";
 
 function App() {
   return (
     <>
       <Canvas camera={{ position: [0, 0, 5] }}>
         <Lights />
-        <Box>
-          <meshStandardMaterial color={"hotpink"} />
-        </Box>
+        <Configurator />
         <OrbitControls enablePan={true} enableRotate={true} />
       </Canvas>
     </>
---
+++ src/components/Configurator.tsx
@@ -0,0 +1,7 @@
+import { Vase } from "../models/Vases";
+
+const Configurator = () => {
+  return <Vase />;
+};
+
+export default Configurator;
