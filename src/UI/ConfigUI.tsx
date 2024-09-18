b779c46b001e61a2b5f75afc53a1641369bfbcac
Tony Glover <tony@drt-software.com>
2024-09-18 22:40:51 +0100

    Added button group

--- src/UI/ConfigUI.tsx
+++ src/UI/ConfigUI.tsx
@@ -1,4 +1,4 @@
-import { useRef } from "react";
+import { useRef, useState, MouseEvent } from "react";
 import Checkbox from "@mui/material/Checkbox";
 import {
   FormGroup,
@@ -8,8 +8,11 @@ import {
   RadioGroup,
   Radio,
 } from "@mui/material";
+import ToggleButton from "@mui/material/ToggleButton";
+import ToggleButtonGroup from "@mui/material/ToggleButtonGroup";
 import Switch from "@mui/material/Switch";
 import useStore from "../state/store";
+
 import { ChangeEvent } from "react";
 
 const ConfigUI = () => {
@@ -19,6 +22,14 @@ const ConfigUI = () => {
   const setCurrentPattern = useStore((state) => state.setCurrentPattern);
   const setCurrentTable = useStore((state) => state.setCurrentTable);
   const switchDayMode = useStore((state) => state.switchDayMode);
+  const [alignment, setAlignment] = useState<string | null>("left");
+
+  const handleAlignment = (
+    event: MouseEvent<HTMLElement>,
+    newAlignment: string | null
+  ) => {
+    setAlignment(newAlignment);
+  };
 
   const toggleRotate = () => {
     setRotating(!isRotating);
@@ -48,49 +59,31 @@ const ConfigUI = () => {
           />
         </FormGroup>
       </div>
+
       <div id="pattern" className="panel">
-        <FormGroup>
-          <FormControl sx={{ mt: 3 }}>
-            <FormLabel
-              sx={{ fontWeight: "bold", fontSize: "h6.fontSize" }}
-              id="demo-radio-buttons-group-label"
-            >
-              Pattern:
-            </FormLabel>
-            <RadioGroup
-              aria-labelledby="demo-radio-buttons-group-label"
-              defaultValue="ink"
-              name="radio-buttons-group"
-              onChange={updatePattern}
-            >
-              <FormControlLabel
-                value="ink"
-                control={<Radio />}
-                label="Ink blot"
-              />
-              <FormControlLabel
-                value="zebra"
-                control={<Radio />}
-                label="Zebra"
-              />
-              <FormControlLabel
-                value="flowers"
-                control={<Radio />}
-                label="Flowers"
-              />
-              <FormControlLabel
-                value="orange"
-                control={<Radio />}
-                label="Orange"
-              />
-              <FormControlLabel
-                value="glass"
-                control={<Radio />}
-                label="Glass"
-              />
-            </RadioGroup>
-          </FormControl>
-        </FormGroup>
+        <ToggleButtonGroup
+          orientation="vertical"
+          value={alignment}
+          exclusive
+          onChange={handleAlignment}
+          aria-label="text alignment"
+        >
+          <ToggleButton sx={{ mb: 2 }} value="ink" aria-label="ink blot">
+            Ink blot
+          </ToggleButton>
+          <ToggleButton sx={{ mb: 2 }} value="zebra" aria-label="zebra">
+            Zebra
+          </ToggleButton>
+          <ToggleButton sx={{ mb: 2 }} value="flowers" aria-label="flowers">
+            Flowers
+          </ToggleButton>
+          <ToggleButton sx={{ mb: 2 }} value="orange" aria-label="orange">
+            Orange
+          </ToggleButton>
+          <ToggleButton sx={{ mb: 2 }} value="glass" aria-label="glass">
+            Glass
+          </ToggleButton>
+        </ToggleButtonGroup>
       </div>
       <div id="table" className="panel">
         <FormGroup>
